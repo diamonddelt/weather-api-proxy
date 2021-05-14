86de96c071b6d2c6a3829f2e492b7a94cf9a0bbd
diamonddelt <rrrasti@yahoo.com>
2021-05-14 14:36:43 -0500

    move tests inline with main app

---
+++ app/test_main.py
@@ -0,0 +1,16 @@
+import pytest
+import main
+
+def test_example_method():
+    a = 1
+    b = 2
+    assert a + 1 == b, f"Test Failed: {a} + 1 != {b}"
+
+# Positive API Tests
+
+def test_debug():
+    assert main.debug() == "<p>Hello, World!</p>"
+
+def test_health_check():
+    assert main.health_check() == {"status": "ok"}
+
